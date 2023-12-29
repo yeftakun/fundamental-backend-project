671da071360ad4072796b17e147b4379b6907703
Yefta Asyel <yeftakun34@gmail.com>
2023-12-29 19:12:48 +0800

    Update NotesService.js

--- notes-app-back-end/src/services/inMemory/NotesService.js
+++ notes-app-back-end/src/services/inMemory/NotesService.js
@@ -1,4 +1,5 @@
 const { nanoid } = require('nanoid');
+// const note = this._notes.filter((n) => n.id === id)[0];
  
 class NotesService {
   constructor() {
@@ -15,5 +16,54 @@ class NotesService {
     };
  
     this._notes.push(newNote);
+ 
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+ 
+    if (!isSuccess) {
+      throw new Error('Catatan gagal ditambahkan');
+    }
+ 
+    return id;
   }
+
+  getNotes() {
+    return this._notes;
+  }
+
+  getNoteById(id) {
+    const note = this._notes.filter((n) => n.id === id)[0];
+    if (!note) {
+      throw new Error('Catatan tidak ditemukan');
+    }
+    return note;
+  }
+  
+  editNoteById(id, { title, body, tags }) {
+    const index = this._notes.findIndex((note) => note.id === id);
+ 
+    if (index === -1) {
+      throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+ 
+    const updatedAt = new Date().toISOString();
+ 
+    this._notes[index] = {
+      ...this._notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+  }
+
+  deleteNoteById(id) {
+    const index = this._notes.findIndex((note) => note.id === id);
+    if (index === -1) {
+      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+    }
+    this._notes.splice(index, 1);
+  }
+
 }
+
+module.exports = NotesService;
