29ceb181d60cd071cf832b0074875fdaa695e5e9
Yefta Asyel <yeftakun34@gmail.com>
2023-12-30 10:20:54 +0800

    finishing...

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -1,22 +1,28 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
-
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+ 
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server({
     port: 3000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-        cors: {
-            origin: ['*'],
-        },
-        security: false,
+      cors: {
+        origin: ['*'],
+      },
     },
   });
-
-  server.route(routes);
-
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+ 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
